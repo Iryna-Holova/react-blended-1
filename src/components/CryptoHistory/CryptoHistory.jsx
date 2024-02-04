c0b8e501003df937e7a9cc2f6cb0bd68f34dfdb9
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2024-02-04 12:40:40 +0200

    task-4

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,28 @@
+import { formatDate } from 'helpers/format-date';
 import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
 
-export const CryptoHistory = () => {
-  return <div>CryptoHistory</div>;
+export const CryptoHistory = ({ transactions }) => {
+  return (
+    <BaseTable>
+      <THead>
+        <tr>
+          <Th>№</Th>
+          <Th>PRICE</Th>
+          <Th>AMOUNT</Th>
+          <Th>DATE</Th>
+        </tr>
+      </THead>
+
+      <tbody>
+        {transactions.map(({ id, price, amount, date }, index) => (
+          <Tr key={id}>
+            <Td>{index + 1}</Td>
+            <Td>{price}</Td>
+            <Td>{amount}</Td>
+            <Td>{formatDate(date)}</Td>
+          </Tr>
+        ))}
+      </tbody>
+    </BaseTable>
+  );
 };
